6aada44f712e1c33a568ba7eb00b54703fabb421
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 01:29:10 -0300

    Updating routes for product CRUD

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
-from api.views.products_views import RegisterProductView, ListProductsView, ProductDetailsView
+from api.views.products_views import (EditUserProductView, ListProductsView,
+                                      ProductDetailsView, UserProductsView)
 from api.views.users_views import SignInView, SignUpView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -14,7 +15,8 @@ urlpatterns = [
     #
     # Products
     #
-    path('products', ListProductsView.as_view(), name='products'),
-    path('products/new', RegisterProductView.as_view(), name='new-product'),
+    path('products', UserProductsView.as_view(), name='user-products'),
+    path('products/list', ListProductsView.as_view(), name='products'),
+    path('products/<int:product_id>', EditUserProductView.as_view(), name='edit-user-product'),
     path('products/<int:product_id>/details', ProductDetailsView.as_view(), name='product-details'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
