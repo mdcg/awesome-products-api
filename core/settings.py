7af80ac17a61d5780b2bcd8bf68632938e255b46
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 00:22:29 -0300

    Updating project media files

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240 # higher than the count of fields
