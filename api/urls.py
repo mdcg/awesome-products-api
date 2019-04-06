d337f47eeed160e5d89a2ba1986bfe641de2e39a
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 00:23:20 -0300

    Creating route for product registration

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,18 @@
+from api.views.products_views import RegisterProductView
 from api.views.users_views import SignInView, SignUpView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 urlpatterns = [
+    #
+    # User
+    #
     path('signup', SignUpView.as_view(), name='signup'),
     path('signin', SignInView.as_view(), name='signin'),
-]
+
+    #
+    # Products
+    #
+    path('products/new', RegisterProductView.as_view(), name='new-product'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
