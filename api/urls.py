8feda9dd8b77d9a693e8688b4f29d3df9cb79099
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 14:56:24 -0300

    Creating routes for basket

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,19 @@
+from api.views.baskets_views import UserBasketProductsView, UserBasketView
 from api.views.products_views import (EditUserProductView, ListProductsView,
                                       ProductDetailsView, UserProductsView)
 from api.views.users_views import SignInView, SignUpView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Awesome Products API')
 
 urlpatterns = [
+    #
+    # Swagger API Docs
+    #
+    path('docs', schema_view, name='docs'),
     #
     # User
     #
@@ -19,4 +27,9 @@ urlpatterns = [
     path('products/list', ListProductsView.as_view(), name='products'),
     path('products/<int:product_id>', EditUserProductView.as_view(), name='edit-user-product'),
     path('products/<int:product_id>/details', ProductDetailsView.as_view(), name='product-details'),
+    #
+    # Basket
+    #
+    path('basket/', UserBasketView.as_view(), name='user-basket'),
+    path('basket/product/<int:product_id>', UserBasketProductsView.as_view(), name='user-basket-products'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
