1967516d65fa344a807be741e07b2104d185bb84
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 01:28:46 -0300

    Updating view for product CRUD

--- api/views/products_views.py
+++ api/views/products_views.py
@@ -9,9 +9,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class RegisterProductView(APIView):
+class UserProductsView(APIView):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
+
+    def get(self, request, format=None):
+        products = request.user.products.all()
+        serialized_products = ProductSerializer(products, many=True)
+
+        response_data = {
+            'status': 'success',
+            'message': None,
+            'data': serialized_products.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         user = request.user
@@ -37,6 +49,89 @@ class RegisterProductView(APIView):
         }
 
         return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EditUserProductView(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id, user=request.user)
+        except ObjectDoesNotExist:
+            response_data = {
+                'status': 'fail',
+                'message': 'Product not found.',
+                'data': None
+            }
+
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+
+        serialized_product = ProductSerializer(product)
+
+        response_data = {
+            'status': 'success',
+            'message': None,
+            'data': serialized_product.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+    def put(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id, user=request.user)
+        except ObjectDoesNotExist:
+            response_data = {
+                'status': 'fail',
+                'message': 'Product not found.',
+                'data': None
+            }
+
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+
+        product_to_update = ProductSerializer(
+            product, data=request.data, partial=True)
+
+        if product_to_update.is_valid():
+            product_to_update.save()
+
+            response_data = {
+                'status': 'success',
+                'message': None,
+                'data': product_to_update.data
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        response_data = {
+            'status': 'fail',
+            'message': 'Something went wrong!',
+            'data': product_to_update.errors,
+        }
+
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, product_id):
+        try:
+            product = Product.objects.get(id=product_id, user=request.user)
+        except ObjectDoesNotExist:
+            response_data = {
+                'status': 'fail',
+                'message': 'Product not found.',
+                'data': None
+            }
+
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+
+        product.delete()
+
+        response_data = {
+            'status': 'success',
+            'message': 'Product successfully deleted.',
+            'data': None
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
 
 
 class ListProductsView(APIView):
@@ -67,8 +162,9 @@ class ProductDetailsView(APIView):
             return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
         serialized_product = ProductSerializer(product)
+
         response_data = {
-            'status': 'succcess',
+            'status': 'success',
             'message': None,
             'data': serialized_product.data
         }
