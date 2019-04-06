4835019b4e598da09fcb006ee8c60e5a97fce62f
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-04-06 14:55:45 -0300

    Creating view for basket

---
+++ api/views/baskets_views.py
@@ -0,0 +1,83 @@
+from api.models import Basket, Product
+from api.serializers.baskets_serializers import BasketSerializer
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class UserBasketView(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get(self, request, format=None):
+        user_basket = request.user.basket
+        serialized_basket = BasketSerializer(user_basket)
+
+        response_data = {
+            'status': 'success',
+            'message': None,
+            'data': serialized_basket.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+
+
+class UserBasketProductsView(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+
+    def post(self, request, product_id, format=None):
+        try:
+            product = Product.objects.get(id=product_id)
+        except ObjectDoesNotExist:
+            response_data = {
+                'status': 'fail',
+                'message': 'Product not found.',
+                'data': None
+            }
+
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+
+        user_basket = request.user.basket
+        user_basket.products.add(product)
+        user_basket.save()
+
+        serialized_basket = BasketSerializer(user_basket)
+
+        response_data = {
+            'status': 'success',
+            'message': 'Product added to the basket successfully.',
+            'data': serialized_basket.data
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, product_id, format=None):
+        try:
+            product = Product.objects.get(id=product_id)
+        except ObjectDoesNotExist:
+            response_data = {
+                'status': 'fail',
+                'message': 'Product not found.',
+                'data': None
+            }
+
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
+
+        user_basket = request.user.basket
+        user_basket.products.remove(product)
+        user_basket.save()
+
+        serialized_basket = BasketSerializer(user_basket)
+
+        response_data = {
+            'status': 'success',
+            'message': 'Product successfully removed from cart.',
+            'data': serialized_basket.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
